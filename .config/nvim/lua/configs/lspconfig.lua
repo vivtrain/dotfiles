7b684a9465a3dc02f0f1b35ec5d7d28039916152
Vivek Kumar <k99vivek@gmail.com>
2025-05-08 14:47:55 -0700

    Update plugins; Add Surround

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,8 +1,7 @@
--- load defaults i.e lua_lsp
+-- load defaults
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- configuring single server, example: typescript
@@ -11,6 +10,21 @@ local nvlsp = require "nvchad.configs.lspconfig"
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+
+lspconfig.lua_ls.setup({
+  cmd = {"lua-language-server"},
+  filetypes = {"lua"},
+  root_dir = lspconfig.util.root_pattern(
+    "~/.local/share/nvim/lazy/nvim-lspconfig/.luarc.json",
+    "~/.local/share/nvim/lazy/nvim-lspconfig/.luarc.jsonc",
+    "~/.local/share/nvim/lazy/nvim-lspconfig/.luacheckrc",
+    "~/.local/share/nvim/lazy/nvim-lspconfig/.stylua.toml",
+    "~/.local/share/nvim/lazy/nvim-lspconfig/stylua.toml",
+    "~/.local/share/nvim/lazy/nvim-lspconfig/selene.toml",
+    "~/.local/share/nvim/lazy/nvim-lspconfig/selene.yml",
+    "~/.local/share/nvim/lazy/nvim-lspconfig/.git"),
+  single_file_support = true
+})
 
 lspconfig.pyright.setup({
   on_init = nvlsp.on_init,
@@ -26,7 +40,7 @@ lspconfig.clangd.setup({
   filetypes = { "cpp", "c", "cc", "h", "tpp", "mpp" },
 })
 
-lspconfig.tsserver.setup({
+lspconfig.ts_ls.setup({
   on_init = nvlsp.on_init,
   on_attach = nvlsp.on_attach,
   capabilities = nvlsp.capabilities,
