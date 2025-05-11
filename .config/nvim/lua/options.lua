58dd5197025361b1fd6516b771665b49dcd0b65b
Vivek Kumar <k99vivek@gmail.com>
2025-05-10 17:40:51 -0700

    Add startup behavior

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -14,20 +14,28 @@ vim.g.clipboard = {
   cache_enabled = true,
 }
 
-local options = vim.o
+local o = vim.o
 -- Do not share unnamed register with system clipboard
-options.clipboard = ""
+o.clipboard = ""
 -- Set a marker for keeping lines under 80 characters
-options.colorcolumn = "81"
+o.colorcolumn = "81"
 -- Enable CursorLine
 -- options.cursorlineopt = 'both'
+o.matchpairs = '[:],(:),{:},<:>'
 
 vim.api.nvim_set_hl(0, "@comment.lua", { link = "Comment" })
 
-vim.api.nvim_create_autocmd({
-  "VimEnter" },
-  { callback = function()
-    require("nvim-tree.api").tree.open()
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  desc = "Open nvim-tree on startup if a buffer is opened (i.e. no NvDash)",
+  callback = function()
+    if vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()):len() > 0 then
+      require("nvim-tree.api").tree.toggle({ find_file = true, focus = false, })
+    end
   end,
 })
 
+vim.api.nvim_create_autocmd({'BufWinEnter'}, {
+  desc = 'Return cursor to where it was last time closing the file',
+  pattern = '*',
+  command = 'silent! normal! g`"zv',
+})
