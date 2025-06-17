5f70c94ff6c3f5e83e85d2e8e0a9d5316073c9de
Vivek Kumar <k99vivek@gmail.com>
2025-06-16 19:03:03 -0700

    try and fix cpair bug where focus gets stolen to signature help

--- .config/nvim/lua/util/cpair.lua
+++ .config/nvim/lua/util/cpair.lua
@@ -2,19 +2,25 @@ local M = {}
 
 function M.is_template()
 
-  local line = vim.api.nvim_get_current_line()
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  local buf = vim.api.nvim_get_current_buf()
+  local win = vim.api.nvim_get_current_win()
+  local line = vim.api.nvim_buf_get_lines(buf, row - 1, row, false)[1]
+  print(line)
   local r, c = unpack(vim.api.nvim_win_get_cursor(0))
   if not (vim.o.filetype == "cpp" or vim.o.filetype == "c") then
     line = line:sub(1, c) .. "<" .. line:sub(c + 1)
-    vim.api.nvim_set_current_line(line)
-    vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+    -- vim.api.nvim_set_current_line(line)
+    vim.api.nvim_buf_set_lines(buf, row - 1, row, false, {line})
+    vim.api.nvim_win_set_cursor(win, { r, c + 1 })
     return
   end
 
   if vim.fn.match({ line }, "template") == 0 then
     line = line:sub(1, c) .. "<>" .. line:sub(c + 1)
-    vim.api.nvim_set_current_line(line)
-    vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+    -- vim.api.nvim_set_current_line(line)
+    vim.api.nvim_buf_set_lines(buf, row - 1, row, false, {line})
+    vim.api.nvim_win_set_cursor(win, { r, c + 1 })
     return
   end
 
@@ -24,22 +30,25 @@ function M.is_template()
       line = line:sub(1, c) .. " " .. line:sub(c + 1)
       c = c + 1
     end
-    vim.api.nvim_set_current_line(line)
-    vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+    -- vim.api.nvim_set_current_line(line)
+    vim.api.nvim_buf_set_lines(buf, row - 1, row, false, {line})
+    vim.api.nvim_win_set_cursor(win, { r, c + 1 })
     return
   end
   if vim.fn.match({ line:sub(0, c) }, "cast\\s*$") == 0 then
     -- c - 1 = 2 chars before the cursor
     line = line:sub(1, c) .. "<>" .. line:sub(c + 1)
-    vim.api.nvim_set_current_line(line)
-    vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+    -- vim.api.nvim_set_current_line(line)
+    vim.api.nvim_buf_set_lines(buf, row - 1, row, false, {line})
+    vim.api.nvim_win_set_cursor(win, { r, c + 1 })
     return
   end
 
 
   line = line:sub(1, c) .. "<" .. line:sub(c + 1)
-  vim.api.nvim_set_current_line(line)
-  vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+  -- vim.api.nvim_set_current_line(line)
+  vim.api.nvim_buf_set_lines(buf, row - 1, row, false, {line})
+  vim.api.nvim_win_set_cursor(win, { r, c + 1 })
   vim.cmd("redraw") -- redraw to add the first <
 
   local old_handler = vim.lsp.handlers["textDocument/signatureHelp"]
@@ -48,7 +57,8 @@ function M.is_template()
       local functionsig = info.signatures[1].label
       if vim.fn.match({ functionsig }, "^\\w\\+<") == 0 then
         -- c + 1 is after including the openning pair very shady code lol
-        vim.api.nvim_set_current_line(line:sub(0, c + 1) .. ">" .. line:sub(c + 2))
+        line = line:sub(0, c + 1) .. ">" .. line:sub(c + 2)
+        vim.api.nvim_buf_set_lines(buf, row - 1, row, false, {line})
       end
     end
   end
